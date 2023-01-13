dfd0baac1ae5d02850f6c91d2fa64c2917f66143
Eganin <egorzaharin12@gmail.com>
2023-01-13 18:21:49 +0500

    init ktor

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("io.ktor.plugin") version "2.1.2"
+    kotlin("plugin.serialization") version "1.8.0"
+}
+
+group = "ru.playzone"
+version = "0.0.1"
+application {
+    mainClass.set("ru.playzone.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ru.playzone.ktor-sample"
